6d39d5151251dfc0bd27caaefe697009130e3797
Kevin Chen <kevinjchen94@gmail.com>
2023-07-18 12:37:20 -0500

    implement pagination and fetching (#2)

--- api/examples/client.rs
+++ api/examples/client.rs
@@ -4,17 +4,18 @@ use space_traders_api::types::FactionSymbol;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let api = match std::env::var("AUTH_TOKEN") {
+    let mut api = match std::env::var("AUTH_TOKEN") {
         Ok(auth_token) => {
             println!("auth token found! instantiating API");
             SpaceTradersApi::new(&auth_token)
-        },
+        }
         Err(_) => {
             println!("auth token not found! registering new");
-            SpaceTradersApi::register("bear", FactionSymbol::Cosmic).await?
+            SpaceTradersApi::register("bear", FactionSymbol::from("COSMIC")).await?
         }
     };
 
-    println!("{:?}", api.agent().await?);
+    api.hydrate().await?;
+    println!("factions available: {:?}", api.faction_symbols());
     return Ok(());
 }
